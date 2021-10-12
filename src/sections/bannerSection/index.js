59e02350aeb03d45192acfc52bdce506c5236d65
blockchainer2022 <=blockchainer.2202@gmail.com>
2021-10-12 18:54:39 +0530

    added autoplay

--- src/sections/bannerSection/index.js
+++ src/sections/bannerSection/index.js
@@ -7,7 +7,7 @@ const Index = () => {
           <iframe
             width="100%"
             height="100%"
-            src="https://www.youtube.com/embed/1jzROE6EhxM?autoplay=1"
+            src="https://www.youtube.com/embed/1jzROE6EhxM?autoplay=1&cc_load_policy=1"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
